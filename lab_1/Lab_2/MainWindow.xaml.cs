68d79a55729c650d0d1c71a0e270abe7fc57c4b9
Pavel E <okis_xxx@mail.ru>
2021-09-18 23:13:48 +0300

    finish second lab

--- lab_1/Lab_2/MainWindow.xaml.cs
+++ lab_1/Lab_2/MainWindow.xaml.cs
@@ -26,7 +26,21 @@ namespace Lab_2
         }
 
         private double Func(double X) {
-            return Math.Pow(Math.E, X);
+            if ((bool)radioTypeFunc1.IsChecked)
+            {
+                return Math.Sinh(X);
+            }
+            else
+            if ((bool)radioTypeFunc2.IsChecked)
+            {
+                return Math.Pow(X, 2);
+            }
+            else
+            if ((bool)radioTypeFunc3.IsChecked)
+            {
+                return Math.Pow(Math.E, X);
+            }
+            else return 0;
         }
 
         
@@ -44,13 +58,18 @@ namespace Lab_2
                 //firstPart = ((Math.Pow(X, (Y + 1))) + (Math.Pow(Math.E, (Y - 1)))) / ((1 + X * (Math.Abs(Y - Math.Tan(Z)))));
                 //secondPart = 1 + Math.Abs(Y - X);
                 //thirdPart = ((Math.Pow(Math.Abs(Y - X), 2)) / 2) - ((Math.Pow(Math.Abs(Y - X), 3)) / 3);
-                result = firstPart * secondPart + thirdPart;
+                result = Math.Max(Math.Min(Func(X), Y), Z);
                 resultBox.Text = result.ToString();
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
+        }
+
+        private void inputX_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
