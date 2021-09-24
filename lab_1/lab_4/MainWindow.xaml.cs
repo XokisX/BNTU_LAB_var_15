6f8262d7cf09b846c99dc628f597979ccf6095cd
Pavel E <okis_xxx@mail.ru>
2021-09-24 16:35:01 +0300

    lab 4 start

---
+++ lab_1/lab_4/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab_4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DataGridTextColumn textColumn = new DataGridTextColumn();
+            textColumn.Header = "First Name";
+            textColumn.Binding = new Binding("FirstName");
+            matrixGrid.Columns.Add(textColumn);
+
+            DataGridRow textRow = new DataGridRow();
+            textRow.Header = "First Name";
+            matrixGrid.CanUserAddRows = true;
+        }
+    }
+}
