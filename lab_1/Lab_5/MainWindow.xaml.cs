1ee882cd1b50246ee90989c4a1b2583d0b0c225d
Pavel E <okis_xxx@mail.ru>
2021-10-09 15:39:31 +0300

    finish 6]

---
+++ lab_1/Lab_5/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private List<string> GetWords(string str) {
+            List<string> list = new List<string>();
+            string word = "";
+            for (short i = 0;i< str.Length;i++) {
+                if (str[i] != ' ')
+                {
+                    word += str[i];
+                }
+                else
+                {
+                    if (word.Length !=0) {
+                        list.Add(word);
+                    }
+                    word = "";
+                }
+                if (word.Length!=0&&i==str.Length-1) {
+                    list.Add(word);
+                }
+            }
+            return (list);
+        }
+
+        private string GetShortWord(List<string> list) {
+            string word=list[0];
+            for (short i = 0;i<list.Count;i++) {
+                if (word.Length>list[i].Length) {
+                    word = list[i];
+                }
+            }
+            return word;
+        }
+
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ResultBox.Text = "";
+            try {
+                List<string> list = GetWords(InputText.Text);
+                string word = GetShortWord(list);
+                ResultBox.Text=$"Word {word} length: {word.Length} index: {list.IndexOf(word)}";
+                
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ lab_1/Lab_6/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Ticket> list = new List<Ticket>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeButtonVisible() {
+            if(ButtonWrite.Visibility==Visibility.Hidden) ButtonWrite.Visibility = Visibility.Visible;
+            else ButtonWrite.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ChangeButtonVisible();
+                string result = File.ReadAllText(openFileDialog.FileName);
+                using (StreamReader streamReader = new StreamReader(openFileDialog.FileName))
+                {
+                    string currentString;
+                    while ((currentString = streamReader.ReadLine()) !=null) {
+                        //MessageBox.Show(currentString);
+                    }
+                }
+            }
+        }
+
+        private void ButtonWrite_Click(object sender, RoutedEventArgs e)
+        {
+            if (InputName.Text != "" && InputMark.Text != "" && InputDate.SelectedDate != null && InputStatus.SelectedItem != null) {
+                list.Add(new Ticket(InputName.Text, InputMark.Text,InputDate.SelectedDate.ToString(), InputStatus.SelectedItem.ToString()));
+;           }
+            ResultGrid.ItemsSource = list;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //save button
+        }
+    }
+}
